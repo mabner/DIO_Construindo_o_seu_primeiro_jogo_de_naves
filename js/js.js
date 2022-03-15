f1fdc0dc9d1bd2099dbd69bb0db79508468de3be
Marcos Leite <m.abner@live.co.uk>
2022-03-15 12:59:48 -0300

    adds shooting  functionality

--- js/js.js
+++ js/js.js
@@ -8,6 +8,7 @@ function start() {
 	$('#fundoGame').append("<div id='amigo' class='anima3'></div>");
 
 	//Principais variáveis do jogo
+	var podeAtirar = true;
 	var jogo = {};
 	var velocidade = 5;
 	var posicaoY = parseInt(Math.random() * 334);
@@ -38,6 +39,7 @@ function start() {
 		moveinimigo1();
 		moveinimigo2();
 		moveamigo();
+		colisao();
 	} // Fim da função loop()
 
 	//Função que movimenta o fundo do jogo
@@ -69,6 +71,7 @@ function start() {
 
 		if (jogo.pressionou[TECLA.D]) {
 			//Chama função Disparo
+			disparo();
 		}
 	} // fim da função movejogador()
 
@@ -101,4 +104,33 @@ function start() {
 			$('#amigo').css('left', 0);
 		}
 	} // fim da função moveamigo()
+
+	function disparo() {
+		if (podeAtirar == true) {
+			// makes it falso so it can't shoot again until the bullet is gone
+			podeAtirar = false;
+
+			topo = parseInt($('#jogador').css('top'));
+			posicaoX = parseInt($('#jogador').css('left'));
+			tiroX = posicaoX + 190;
+			topoTiro = topo + 37;
+			$('#fundoGame').append("<div id='disparo'></div");
+			$('#disparo').css('top', topoTiro);
+			$('#disparo').css('left', tiroX);
+
+			var tempoDisparo = window.setInterval(executaDisparo, 30);
+		} //Fecha podeAtirar
+
+		function executaDisparo() {
+			posicaoX = parseInt($('#disparo').css('left'));
+			$('#disparo').css('left', posicaoX + 15);
+
+			if (posicaoX > 900) {
+				window.clearInterval(tempoDisparo);
+				tempoDisparo = null;
+				$('#disparo').remove();
+				podeAtirar = true;
+			}
+		} // Fecha executaDisparo()
+	} // Fecha disparo()
 } // Fim da função start
